7f32cfb35676136917aabc5a7ffeace6bfb7b796
tegareyy <tegarirfanhamid27@gmail.com>
2025-01-30 07:40:03 +0700

    Exercise 1-2-3 Done :)

--- session-03.ts
+++ session-03.ts
@@ -27,10 +27,7 @@ console.log(cmToKm(100000));
 // Write a code to format number as currency (IDR)
 /* Example : 1000 → “Rp. 1.000,00” */
 function formatToIDR(amount: number): string {
-  const format = amount.toLocaleString("id-ID", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
+  const format = amount.toLocaleString("id-ID", {minimumFractionDigits: 2, maximumFractionDigits: 2,});
   return `Rp. ${format}`;
 }
 console.log(formatToIDR(1000));
@@ -46,3 +43,60 @@ console.log(removeOccu("Hello World", "ell"));
 
 // Write a code to capitalize the first letter of each word in a string
 /* Example : “hello world” → “Hello World” */
+function capitalWords(str: string): string {
+  return str.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+}
+console.log(capitalWords("hello World"))
+
+// Write a code to swap the case of each character from string
+/* Example : ‘The QuiCk BrOwN Fox’ -> ‘ tHE qUIcK bRoWn fOX’ */
+function swapCase(str: string): string {
+  return str.split("").map((char) => char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()).join("");
+}
+console.log(swapCase("The QuiCk BrOwN Fox"));
+
+// Write a code to find the largest of two given integers
+/* Example : num1 = 42, num2 = 27 → 42 */
+function findLargest(num1: number, num2: number): number {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+console.log(findLargest(42, 27));
+
+// Write a conditional statement to sort three numbers
+/* Example : num1 = 42, num2 = 27, num3 = 18 → 18, 27, 42 */
+function sortThreeNumbers(num1: number, num2: number, num3: number): string {
+  let a = num1,
+    b = num2,
+    c = num3;
+
+  if (a > b) [a, b] = [b, a];
+  if (a > c) [a, c] = [c, a];
+  if (b > c) [b, c] = [c, b];
+
+  return `${a}, ${b}, ${c}`;
+}
+console.log(sortThreeNumbers(42, 27, 18));
+
+// Write a code that shows 1 if the input is a string, 2 if the input is a number, and 3 for others data type
+/* Example : “hello” → 1 */
+function checkDataType(input: any): number {
+  return typeof input === "string" ? 1 : typeof input === "number" ? 2 : 3;
+}
+console.log(checkDataType("hello"));
+
+// Write a code to change every letter a into * from a string of input
+/* Example : ‘An apple a day keeps the doctor away’ -> `*n *pple * d*y keeps the doctor *w*y` */
+function manualReplaceA(input: string): string {
+  let output = "";
+  for (const char of input) {
+    output += char.toLowerCase() === "a" ? "*" : char;
+  }
+  return output;
+}
+const original = "An apple a day keeps the doctor away";
+const result = manualReplaceA(original);
+console.log(result);
